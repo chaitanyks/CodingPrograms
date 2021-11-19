01af5f0e021a17ccefe065c145f7613dff61d20e
Undefined <defcdr@gmail.com>
2021-11-19 10:36:04 +0530

    Brian Kernighan’s Algorithm

---
+++ Algorithms/Brian Kernighan’s Algorithm.cpp
@@ -0,0 +1,9 @@
+unsigned int countSetBits(int n)
+    {
+        unsigned int count = 0;
+        while (n) {
+            n &= (n - 1);
+            count++;
+        }
+        return count;
+    }
